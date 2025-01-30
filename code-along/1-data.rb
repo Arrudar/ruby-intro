44b1633001fdef7beadfe0a191e1404c622879a7
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-01-30 01:03:01 +0000

    Solve ex. 1 and 2

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,84 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 3
+puts 5
+puts 3+1
+puts 2-4
+puts 1*3
+puts 2**4
+puts 5/2.0
 
 # Non-numbers
+puts ""
+puts ""
+puts ""
+
+puts "Hello, world!"
+puts ""
+puts ""
+puts ""
+puts "Hello, world!" + " Hey Hey!"
+puts "Hello! " * 5
+
+puts "What is this: " + 5.to_s
 
 # True, False
 
+puts true
+puts false
+
 # Nothing
+
+puts nil
+
 
 # Variables
 
+x = 10
+y = 3
+
+puts "x value: "+ x.to_s 
+puts "y value: " + y.to_s
+puts x * y
+puts ""
+puts ""
+
+
 # Combine strings and variables
 
+k = "tacos & "
+v = 3
+
+puts k * v
+
+puts ""
+puts ""
+
+first_name = "Pedro"
+greting = "Hello my friend, #{first_name} !"
+
+puts greting
+
+puts "#{k} * #{v}"
+
+puts ""
+puts ""
 # String manipulation
+puts ""
+puts ""
+
+puts "Hello".length
+puts "how are you today?".capitalize
+puts "computer".reverse
+
+puts ""
+puts ""
+
+creed = "This IS the way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
+
+puts ""
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,56 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+test_is_true = true
+puts "Value true: " + test_is_true.to_s
+
+test_is_false = false
+puts "Value false: " + test_is_false.to_s
 
 # Boolean Expressions
 
+puts 3 == 2
+puts 3 != 2
+puts 4 > 2
+puts 16 < 15.5
+
 # If Conditional Logic
+
+if 3 == 3
+then 
+    puts "uhul :D"
+else 
+    puts "ahnnn :("
+end
 
 # If/Else Conditional Logic
 
 # Elsif Conditional Logic
 
+user_entered_password = "password"
+real_password = "tacos-nachos-gato"
+
+if user_entered_password == real_password
+    puts "welcome my friend! =)"
+else
+    puts "what!? Are you trying to break-in?"
+end
+
+
+minimum_value = 500
+bank_value = 460
+
+if minimum_value == bank_value
+    puts "Chashin!"
+else
+    puts "Not today, how about invest some bucks?!"
+    puts bank_value = bank_value + 7
+    puts "you are missing " + (minimum_value - bank_value).to_s + " dollars" 
+end
+
 # Combining Expressions
+
+#AND
+&&
+
+#OR ||
